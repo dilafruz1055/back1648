b2e0c83e07562d4686d6df2cc24a73e90f2795cc
Dilafruz Shukurova <dilafruzshukurova36@gmail.com>
2024-08-08 20:15:34 +0500

    m

---
+++ month1/lesson1.py
@@ -0,0 +1,14 @@
+print("Dilafruz")
+print(5)
+
+print(10+5) # buni javobi 15 
+# print(20/0) #ERROR
+print(type('100nimadir'))
+
+
+# print - ma'lumotni ekranga/terminalga chiqarib beradi
+# type - funktsiyasi ob'ekt turini qaytaradi yoki o'tkazilgan argumentlar asosida yangi turdagi ob'ektni qaytaradi
+# int - funksiya belgilangan qiymatni butun songa aylantiradi
+# str - belgilangan qiymatni satrga aylantiradi
+# bool - dasturlashda siz ko'pincha ifoda to'g'ri yoki noto'g'ri ekanligini bilishingiz kerak
+# float - belgilangan qiymatni suzuvchi nuqta raqamiga aylantiradi~
